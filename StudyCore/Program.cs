1d30bfebb1d8cb63e1620221441fd3778fd2fed5
DevWiki <devwiki@qq.com>
2020-04-24 14:13:03 +0800

    addwpf

---
+++ StudyCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace StudyCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(DateTime.Now.ToFileTimeUtc());
+
+            if (a > 9)
+            {
+                goto Test;
+            }
+            Console.WriteLine("if ...");
+
+            Test:
+            Console.WriteLine("Test");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string name;
+    }
+}
